5e4738ff6a443b70abbeb9c52c34ce84dd0decba
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-06-09 02:55:40 +0530

    Add destroy  action in user_stocks controller

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -5,9 +5,18 @@ class UserStocksController < ApplicationController
 		if stock.blank? 
 			stock = Stock.new_lookup(params[:ticker_symbol])
 			stock.save
+		end
 			@user_stock = UserStock.create(user: current_user, stock: stock)
 			flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
 			redirect_to my_portfolio_path
-		end
-	end
+  end
+
+  def destroy
+    stock = Stock.find_by(ticker_symbol: params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.name} was successfully removed from portfolio"
+    redirect_to my_portfolio_path
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false if stock == nil
+    stocks.where(id: stock.id).exists?
+  end
+  
+  def under_stock_limit?
+    stocks.count < 10
+  end
+
+  def can_track_stocks?(ticker_symbol)
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)  
+  end
 end
