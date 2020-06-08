9227481d2f6a5f8f4bcc8ec3b119f7607c7d9276
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-06-09 03:54:29 +0530

    Modified the devise vies registration edit and new template

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,8 @@ class User < ApplicationRecord
   def can_track_stocks?(ticker_symbol)
     under_stock_limit? && !stock_already_tracked?(ticker_symbol)  
   end
+
+  def full_name
+    "#{first_name} #{last_name}"    
+  end
 end
