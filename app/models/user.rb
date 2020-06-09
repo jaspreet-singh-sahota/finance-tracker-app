2bdbdbef80c5c04fbe7fbb77f2c7b9fdbb96b70d
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-06-09 23:35:09 +0530

    Add not_friends method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,33 @@ class User < ApplicationRecord
   def full_name
     "#{first_name} #{last_name}"    
   end
+
+  def self.search(params)
+    params.strip!
+    send_back = (email_matches(params) + first_name_matches(params) + last_name_matches(params)).uniq
+  end
+
+  def self.first_name_matches(params)
+    matches('first_name',  params)
+  end
+
+  def self.last_name_matches(params)
+    matches('last_name',  params)
+  end
+
+  def self.email_matches(params)
+    matches('email',  params)
+  end
+
+  def self.matches(key , params)
+    where("#{key} like ?", "%#{params}%")
+  end
+
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
+
+  def not_friends_with?(not_friends)
+    self.friends.where(friend_id: not_friends) 
+  end
 end
